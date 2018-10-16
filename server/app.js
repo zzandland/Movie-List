1a975cb958991dd351efb2a21f8d486bad4a8d4b
Hack Reactor Student <student@hackreactor.com>
2018-10-15 21:48:23 -0700

    setup server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
+const app = express();
+
+app.use(parser.json());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(3000, function() {
+  console.log('Server Initiated');
+})
